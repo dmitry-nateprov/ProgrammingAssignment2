1cd701eb2846f9ec30c27370becd19b799ded1b2
dmitry-nateprov <75201496+dmitry-nateprov@users.noreply.github.com>
2020-11-29 17:03:03 +0300

    I have done this task

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We have 2 functions
+## makeCacheMatrix consists of set, get, set_inv, get_inv
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()){
+    inv <- NULL                                 #initializing inverse as Null
+    set <- function(y){
+        x <<- y
+        inv <- NULL
+    }
+    get <- function() x                         #get matrix x
+    set_inv <- function(inverse) inv <<- inverse
+    get_inv <- function() inv
+    list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
+## This is used to get the cache data
 
+cacheSolve <- function(x, ...) {                #gets cache data
+    inv <- x$get_inv()
+    if(!is.null(inv)){                          #checks inverse is Null or not
+        message("getting cashed result")
+        return(inv)                             #returns inverse value
+    }
+    dat <- x$get()
+    inv <- solve(dat, ...)                      #calculates inverse value
+    x$set_inv(inv)
+    inv                                         #returns inversed "x" matrix
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
